a41e865efc5721eec9a098b644ba45406780a763
gasolineeater <ezekaj@student.42heilbronn.de>
2025-04-08 16:10:58 +0200

    Add: implementation of putstr.

---
+++ exam02/level01/ft_putstr/ft_putstr.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+// int	main()
+// {
+// 	ft_putstr("Elvi Zekaj.");
+// 	return (0);
+// }
