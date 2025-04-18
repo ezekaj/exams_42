7fa5fe8a02b08f4737f2e791ef3f7850acd1371b
gasolineeater <ezekaj@student.42heilbronn.de>
2025-04-18 16:18:03 +0200

    Add: Implementation of inter.c correctly

--- exam02/level02/inter/inter.c
+++ exam02/level02/inter/inter.c
@@ -4,20 +4,24 @@ int	main(int ac, char **av)
 {
 	int	i;
 	int	j;
+	int	lookup[256] = {};
 
 	i = 0;
-	j = 0;
 	if (ac == 3)
 	{
-		while (av[1][i] && av[2][j])
+		while (av[1][i])
 		{
-			if (av[1][i] == av[2][j])
+			j = 0;
+			while (av[2][j])
 			{
-				write(1, &av[1][i], 1);
+				if (av[1][i] == av[2][j] && !lookup[(int)av[2][j]])
+				{
+					lookup[(int)av[2][j]] = 1;
+					write(1, &av[2][j], 1);
+				}
 				j++;
 			}
 			i++;
-			j++;
 		}
 	}
 	write(1, "\n", 1);
